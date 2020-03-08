c01ea7399a40007845726e19d311a2b5106dd137
marcos moura <marcosmoura00@hotmail.com>
2020-03-08 13:43:35 -0300

    Criacao da estrutura DDD inicial do projeto

---
+++ src/main/java/java-project/spring-project-ddd/Application.java
@@ -0,0 +1,10 @@
+package br.java_project.spring_project_ddd;
+
+@SpringBootApplication // that is @Configuration @EnableAutoConfiguration @ComponentScan
+public class Application {
+
+	public static void main(final String[] args) {
+
+	}
+
+}
---
+++ src/main/java/java-project/spring-project-ddd/package-info.java
@@ -0,0 +1,2 @@
+/**A sample project following Domain Driven Design with Spring Data JPA*/
+package br.java_project.spring_project_ddd;
